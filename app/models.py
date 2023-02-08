26368108bdbf0f7bf58270984020a1ec9b640598
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-02-07 21:48:53 -0500

    EMPTY FILES  |  BASIC SETUP-Continued1

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-
-class ChessPlayer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String)
