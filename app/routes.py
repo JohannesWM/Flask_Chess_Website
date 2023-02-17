8ccda3a8b123907dc6fd49d21cc62db1861a4862
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-02-16 22:05:31 -0500

    WORKING WEB | ajax + JQUERY have been used front end to connect user to the flask backend

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,27 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+from chess_pieces import base_piece as bp
+
+piece1 = bp.ChessPiece([0, 0], "white")
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    return render_template('chess.html', position=piece1.position)
 
-    return render_template('index.html')
+
+@app.route("/update_center_text", methods=["POST"])
+def update_center_text():
+    new_text = request.form.get("text")
+    # print(new_text)
+    # perform some operation to update the center text object
+
+    return new_text
+
+@app.route("/update_board", methods=["POST"])
+def update_board():
+    new_text = request.form.get("text")
+    print(new_text)
+    return new_text
+
--- chess_pieces/chess_test.py
+++ chess_pieces/chess_test.py
@@ -1,5 +1,22 @@
 from base_piece import ChessPiece
+import chess
+# board = chess.Board()
+# board.push_san("c3")
+# print(board)
+#
+#
+# basicPiece = ChessPiece([2, 1], "black")
+# basicPiece.move_position([1, 2])
+# print(basicPiece.position)
+move = chess.Move.from_uci("g1f3")
 
-basicPiece = ChessPiece([2, 1], "black")
-basicPiece.move_position([1, 2])
-print(basicPiece.position)
+board = chess.Board()  # importing the python chess libarry
+board.legal_moves  # create a new chess board instance
+board.legal_moves.count()  # returns a dynamic list of legal moves
+board.push(move)  # play a move (transforms teh state of the baord)
+# board.pop()  # removes the last move played
+# board.piece_type_at(square)  # returns the suqr's corresponding chess piece
+print(board.turn)  # returns a chess color object
+
+board
+print(board)
